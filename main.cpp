dd704a423479141c7ae31fcf7b20df4ab6c59aa3
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-04-02 22:43:55 +0700

    feat(main): add template file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+  int angka;
+
+  cout << "Masukan angka : ";
+  cin >> angka;
+
+  cout << "Angka yang dimasukan " << angka << endl;
+
+  return 0;
+}
