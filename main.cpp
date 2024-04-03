218a2eb2caac9f29ecd3584451a6a0988aefad31
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-04-03 15:15:43 +0700

    chore(main): add class and struct

--- main.cpp
+++ main.cpp
@@ -1,16 +1,54 @@
 #include <iostream>
 
-using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
+
+enum Type { Nature = 1, History = 2, Culture = 3 };
+
+struct Destination {
+  string name, description, location, work_hours;
+  int person, price;
+  Type type;
+};
+
+struct Node {
+  Destination data;
+  Node *next, *prev;
+};
+
+class Gitar {
+private:
+  Node *head, *tail, *newNode, *currentNode;
+
+  bool isNodeEmpty() { return (head == nullptr) ? true : false; }
+
+  int totalNodeData() {
+    int total = 0;
+
+    if (isNodeEmpty())
+      return total;
+
+    currentNode = head;
+
+    do {
+      total++;
+      currentNode = currentNode->next;
+    } while (currentNode != head);
+
+    return total;
+  }
+
+public:
+  Gitar() { head = tail = nullptr; }
+
+  void test() { cout << "This is data from gitar class" << endl; }
+};
 
 int main() {
-  int angka;
+  Gitar gitar;
 
-  cout << "Masukan angka : ";
-  cin >> angka;
-
-  cout << "Angka yang dimasukan " << angka << endl;
+  gitar.test();
 
   return 0;
 }
--- run.bat
+++ run.bat
@@ -1,20 +1,21 @@
 echo off
 
 REM Get cpp file name
-set /p file_source=Enter your cpp file: 
+REM set /p file_source=Enter your cpp file: 
 
 REM Print current system status
-echo Compiling cpp file into executable file
+REM echo Compiling cpp file into executable file
 
 REM Call g++ to compile file source name
-g++ -std=c++11 "%file_source%.cpp" -o "%file_source%.exe"
+REM g++ -std=c++11 "%file_source%.cpp" -o "%file_source%.exe"
+g++ -std=c++11 "main.cpp" -o "main.exe"
 
 REM Check if compiling status is success
 if %errorlevel% equ 0 (
   REM If success then we print compiling is success and run it
-  echo Compiling success, Trying to run %file_source%.exe
+  echo Compiling success, Trying to run main.exe
 
-  "%file_source%.exe"
+  "main.exe"
 ) else (
   echo Compiling error, something went wrong
 )
