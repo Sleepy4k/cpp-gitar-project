f6e225df36ff10e71236a7a3a17df5293aeb202b
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-04-08 00:26:35 +0700

    chore(misc): clean up code

--- run.bat
+++ run.bat
@@ -1,13 +1,6 @@
 echo off
 
-REM Get cpp file name
-REM set /p file_source=Enter your cpp file: 
-
-REM Print current system status
-REM echo Compiling cpp file into executable file
-
 REM Call g++ to compile file source name
-REM g++ -std=c++11 "%file_source%.cpp" -o "%file_source%.exe"
 g++ -std=c++11 "main.cpp" -o "main.exe"
 
 REM Check if compiling status is success
