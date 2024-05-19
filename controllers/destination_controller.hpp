635647cc5ae9d65750e578c96215d161dab25b74
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-05-19 22:46:47 +0700

    chore(controller): add code documentation and update code

--- controllers/destination_controller.hpp
+++ controllers/destination_controller.hpp
@@ -37,35 +37,57 @@ using std::string;
  */
 class DestinationController {
 private:
+  // Inisialisasi variabel userData untuk menyimpan data user
+  // dari class UserController
   UserController userData;
+
+  // Inisialisasi variabel destinationList untuk menyimpan data destinasi
+  // dari class LinkedList dengan tipe data DestinationStruct
   List<DestinationStruct> destinationList;
 
+  /**
+   * @brief Membuat form untuk mengisi data destinasi
+   * 
+   * @return DestinationStruct 
+   */
   DestinationStruct form_destination() {
+    // Inisialisasi variabel type dan data untuk menyimpan data
     int type;
+
+    // Inisialisasi variabel data untuk menyimpan data destinasi
     DestinationStruct data;
 
-    // Clear buffer
+    // Jika pada input sebelumnya ada yang tersisa
+    // maka kita akan menghapusnya terlebih dahulu
     if (cin.peek() == '\n') cin.ignore();
 
+    // Meminta input dari user untuk mengisi nama destinasi
     cout << "Masukan nama destinasi : ";
     getline(cin, data.name);
 
+    // Meminta input dari user untuk mengisi deskripsi destinasi
     cout << "Masukan deskripsi destinasi : ";
     getline(cin, data.description);
 
+    // Meminta input dari user untuk mengisi lokasi destinasi
     cout << "Masukan lokasi destinasi : ";
     getline(cin, data.location);
 
+    // Meminta input dari user untuk mengisi jam operasional destinasi
     cout << "Masukan jam operasional : ";
     getline(cin, data.work_hours);
 
+    // Meminta input dari user untuk mengisi jumlah orang per tiket
     cout << "Masukan jumlah orang per tiket : ";
     cin >> data.person;
 
+    // Meminta input dari user untuk mengisi harga per tiket
     cout << "Masukan harga per tiket : ";
     cin >> data.price;
 
+    // Meminta input dari user untuk mengisi jenis destinasi
     cout << "Jenis destinasi" << endl;
+    cout << "==================" << endl;
     cout << "1. Alam" << endl;
     cout << "2. Sejarah" << endl;
     cout << "3. Budaya" << endl;
@@ -73,77 +95,197 @@ private:
     cout << "Masukan jenis destinasi : ";
     cin >> type;
 
+    // Melakukan pengecekan jenis destinasi
     switch (type) {
+    // Jika user memilih 1
     case 1:
+      // Maka jenis destinasi akan menjadi alam
       data.type = Nature;
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih 2
     case 2:
+      // Maka jenis destinasi akan menjadi sejarah
       data.type = History;
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih 3
     case 3:
+      // Maka jenis destinasi akan menjadi budaya
       data.type = Culture;
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih selain 1, 2, 3
     default:
+      // Maka jenis destinasi akan menjadi alam secara default
       cout << "Pilihan tidak valid, jenis wisata akan menjadi alam secara default" << endl;
+
+      // Menghentikan pengecekan
       data.type = Nature;
+
+      // Menghentikan pengecekan
       break;
     }
 
+    // Mengembalikan data yang sudah di isi
     return data;
   }
 
+  /**
+   * @brief Fungsi untuk menambahkan data destinasi wisata
+   * 
+   * @return void
+   */
   void insert_destination() {
+    // Deklarasi varaible position untuk menyimpan posisi data
     int position;
 
+    // Memanggil method form_destination untuk membuat form
+    // dan mengisi dari data destinasi yang akan di inputkan
     DestinationStruct data = form_destination();
+
+    // Memanggil method insert pada destinationList
+    // untuk menambahkan data destinasi pada linked list
     destinationList.push(data);
+
+    // Memanggil method insert pada Destination
+    // untuk menambahkan data destinasi pada file csv
+    DestinationModel::insert(data);
   }
 
+  /**
+   * @brief Menampilkan detail destinasi
+   * 
+   * @return void
+   */
   void show_destination() {
+    // Mencetak judul detail destinasi
     cout << "Detail Destinasi" << endl;
+
+    // Mencetak garis untuk memisahkan antara judul
     cout << "===================" << endl;
 
+    // Memanggil method showNodeData pada destination
+    // untuk menampilkan data destinasi
     destinationList.showNodeData<DestinationStruct>(true);
 
+    // Mencetak garis untuk memisahkan antara data
     cout << "===================" << endl;
+
+    // Menunggu user untuk menekan tombol apapun
+    // untuk melanjutkan proses
     system("pause");
   }
 
+  /**
+   * @brief Mengubah data destinasi
+   * 
+   * @return void
+   */
   void update_destination() {
-    DestinationStruct result, newData = form_destination(), data = destinationList.findNodeData<DestinationStruct>(2);
+    // Deklarasi variabel oldData, newData, dan result
+    // dengan tipe data DestinationStruct
+    // untuk menyimpan data lama, data baru, dan hasil
+    DestinationStruct result, oldData, newData;
 
-    result = destinationList.update(data, newData);
+    // Mencetak judul detail destinasi
+    cout << "Detail Destinasi" << endl;
 
+    // Mencetak garis untuk memisahkan antara judul
+    cout << "===================" << endl;
+
+    // Memanggil method showNodeData pada destination
+    // untuk menampilkan data destinasi
+    oldData = destinationList.showNodeData<DestinationStruct>(true);
+
+    // Mencetak garis untuk memisahkan antara data
+    cout << "===================" << endl;
+
+    // Mencetak judul untuk mengubah destinasi
+    cout << "Ubah Destinasi" << endl;
+
+    // Memanggil method form_destination untuk membuat form
+    // dan mengisi dari data destinasi yang akan di inputkan
+    newData = form_destination();
+
+    // Memanggil method update pada destinationList
+    // untuk mengubah data destinasi pada linked list
+    result = destinationList.update(oldData, newData);
+
+    // Cek apakah hasil data yang di update kosong atau tidak
+    // Jika kosong maka system akan mencetak pesan data tidak ditemukan
+    // dan menghentikan proses
     if (result.name.empty() || result.description.empty() || result.location.empty()) {
+      // Mencetak pesan data tidak ditemukan
       cout << "Data tidak ditemukan" << endl;
+
+      // Menghentikan proses
       return;
     }
 
+    // Memanggil method update pada Destination
     cout << "Data " << result.name << " berhasil diubah" << endl;
+
+    // Menunggu user untuk menekan tombol apapun
     system("pause");
   }
 
+  /**
+   * @brief Menghapus data destinasi
+   * 
+   * @return void
+   */
   void delete_destination() {
+    // Deklarasi variabel data dengan tipe data DestinationStruct
+    // untuk menyimpan data destinasi yang akan dihapus
+    DestinationStruct data = destinationList.showNodeData<DestinationStruct>(true);
+
+    // Deklarasi variabel confirmation
     char confirmation;
 
-    cout << "Apakah kamu yakin mau menghapus? (y/N)" << endl;
+    // Meminta konfirmasi dari user apakah yakin
+    cout << "Apakah kamu yakin mau menghapus? (y/N) : ";
     cin >> confirmation;
 
+    // Jika konfirmasi tidak y maka menghentikan proses
     if (tolower(confirmation) != 'y') return;
 
-    DestinationStruct data = destinationList.findNodeData<DestinationStruct>(2);
-
+    // Cek apakah data yang akan dihapus kosong atau tidak
+    // Jika kosong maka system akan mencetak pesan data tidak ditemukan
+    // dan menghentikan proses
     if (data.name.empty() || data.description.empty() || data.location.empty()) {
+      // Mencetak pesan data tidak ditemukan
       cout << "Data tidak ditemukan" << endl;
+
+      // Menghentikan proses
       return;
     }
 
+    // Memanggil method delete pada Destination untuk menghapus data
+    // pada linked list
     destinationList.removeByData(data);
 
+    // Memanggil method delete pada Destination untuk menghapus data
     cout << "Data " << data.name << " berhasil dihapus" << endl;
+
+    // Menunggu user untuk menekan tombol apapun
     system("pause");
   }
 
+  /**
+   * @brief Menampilkan menu destinasi
+   * 
+   * @param choice
+   * @param index 
+   * @param page 
+   * @param pagination 
+   * @param result 
+   * 
+   * @return bool 
+   */
   bool adminMenu(int *choice, int *index, int *page, int *pagination, PaginationStruct result) {
     cout << "List of Destination Menu" << endl;
     cout << "===================" << endl;
@@ -158,46 +300,102 @@ private:
     cout << "Pilih menu : ";
     cin >> *choice;
 
+    // Cek apakah pilihan user tidak valid
+    // Jika pilhan user kurang dari 1 atau lebih dari 7
+    // maka system akan mencetak pesan pilihan tidak valid
     if (*choice < 1 || *choice > 7) {
+      // Mencetak pesan pilihan tidak valid
       cout << "Pilihan tidak valid" << endl;
+
+      // Menunggu user untuk menekan tombol apapun
       system("pause");
     }
 
+    // Jika user memilih menu 7
     if (*choice == 7) return false;
+
+    // Menghapus layar pada terminal
     system("cls");
 
+    // Melakukan pengecekan pilihan user
     switch (*choice) {
+    // Jika user memilih menu 1
     case 1:
+      // Memanggil method show_destination
+      // untuk menampilkan detail destinasi
       show_destination();
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih menu 2
     case 2:
+      // Memanggil method insert_destination
+      // untuk menambahkan data destinasi
       insert_destination();
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih menu 3
     case 3:
+      // Memanggil method update_destination
+      // untuk mengubah data destinasi
       update_destination();
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih menu 4
     case 4:
+      // Memanggil method delete_destination
+      // untuk menghapus data destinasi
       delete_destination();
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih menu 5
     case 5:
+      // Mengecek apakah data selanjutnya kosong
+      // Jika kosong maka system akan menghentikan proses
       if (!result.next) break;
 
+      // Jika tidak kosong maka system akan menambahkan index dan page
       *index += 1;
-      *page -= *pagination;
+      *page += *pagination;
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih menu 6
     case 6:
+      // Mengecek apakah data sebelumnya kosong
+      // Jika kosong maka system akan menghentikan proses
       if (!result.back) break;
 
+      // Jika tidak kosong maka system akan mengurangi index dan page
       *index -= 1;
       *page -= *pagination;
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih selain 1, 2, 3, 4, 5, 6
     default:
+      // Menghentikan pengecekan
       break;
     }
 
+    // Mengembalikan nilai true
     return true;
   }
 
+  /**
+   * @brief Menampilkan menu destinasi
+   * 
+   * @param choice
+   * @param index 
+   * @param page 
+   * @param pagination 
+   * @param result 
+   * 
+   * @return bool 
+   */
   bool userMenu(int *choice, int *index, int *page, int *pagination, PaginationStruct result) {
     cout << "List of Destination Menu" << endl;
     cout << "===================" << endl;
@@ -209,70 +407,136 @@ private:
     cout << "Pilih menu : ";
     cin >> *choice;
 
+    // Cek apakah pilihan user tidak valid
+    // Jika pilhan user kurang dari 1 atau lebih dari 4
+    // maka system akan mencetak pesan pilihan tidak valid
     if (*choice < 1 || *choice > 4) {
+      // Mencetak pesan pilihan tidak valid
       cout << "Pilihan tidak valid" << endl;
+
+      // Menunggu user untuk menekan tombol apapun
       system("pause");
     }
 
+    // Jika user memilih menu 4
     if (*choice == 4) return false;
+
+    // Menghapus layar pada terminal
     system("cls");
 
+    // Melakukan pengecekan pilihan user
     switch (*choice) {
+    // Jika user memilih menu 1
     case 1:
+      // Memanggil method show_destination
+      // untuk menampilkan detail destinasi
       show_destination();
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih menu 2
     case 2:
+      // Mengecek apakah data selanjutnya kosong
+      // Jika kosong maka system akan menghentikan proses
       if (!result.next) break;
 
+      // Jika tidak kosong maka system akan menambahkan index dan page
       *index += 1;
       *page += *pagination;
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih menu 3
     case 3:
+      // Mengecek apakah data sebelumnya kosong
+      // Jika kosong maka system akan menghentikan proses
       if (!result.back) break;
 
+      // Jika tidak kosong maka system akan mengurangi index dan page
       *index -= 1;
       *page -= *pagination;
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih selain 1, 2, 3
     default:
+      // Menghentikan pengecekan
       break;
     }
 
+    // Mengembalikan nilai true
     return true;
   }
 
 public:
+  /**
+   * @brief Inisialisasi data destinasi
+   * 
+   * @return void
+   */
   void init() {
+    // Mengecek apakah file destination_data.csv tidak ada
+    // Jika tidak ada maka system akan membuat file destination_data.csv
+    // dan menambahkan data destinasi secara default
     if (!FileStorage::Exists(Path::getPath() + "/data/destination_data.csv")) {
+      // Membuat file destination_data.csv
       FileStorage::Create(Path::getPath() + "/data/destination_data.csv");
+
+      // Menambahkan data destinasi secara default
       DestinationModel::insert(DestinationStruct{"Taman Nasional Bromo Tengger Semeru", "Taman Nasional Bromo Tengger Semeru adalah sebuah kawasan konservasi alam di Jawa Timur", "Probolinggo Jawa Timur", "24 Jam", 1, 50000, Nature});
       DestinationModel::insert(DestinationStruct{"Candi Borobudur", "Candi Borobudur adalah sebuah candi Buddha yang terletak di Magelang Jawa Tengah", "Magelang Jawa Tengah", "06.00 - 17.00", 1, 50000, History});
     }
 
+    // Mengecek apakah data destinasi kosong pada linked list
+    // Jika kosong maka system akan membaca data dari file csv
     if (destinationList.isEmpty()) {
+      // Membaca data dari file csv
       std::vector<DestinationStruct> data = DestinationModel::read();
 
+      // Melakukan perulangan untuk menambahkan data ke dalam linked list
+      // berdasarkan data yang sudah di baca dari file csv
       for (int i = 0; i < data.size(); i++) {
+        // Menambahkan data ke dalam linked list
         destinationList.push(data[i]);
       }
     }
   }
 
+  /**
+   * @brief Menampilkan menu destinasi
+   * 
+   * @param user 
+   * 
+   * @return void
+   */
   void menu(UserController user) {
+    // Mengisi data user dari class UserController
     userData = user;
 
+    // Deklarasi variabel isRunning, choice, index, page, pagination
     bool isRunning = true;
     int choice, index = 1, page = 5, pagination = 5;
 
     do {
+      // Menghapus layar pada terminal
       system("cls");
 
+      // Mencetak data dari pagination
       cout << "Menampilkan list dari " << page - pagination << " - " << page << " data" << endl;
 
+      // Memanggil method showAllNodes pada destination
+      // untuk menampilkan semua data destinasi pada linked list
       PaginationStruct result = destinationList.showAllNodes(index, pagination, false);
 
+      // Mencetak garis untuk memisahkan antara data
+      cout << endl;
+
+      // Melakukan pengecekan apakah user adalah admin atau tidak
+      // Jika user adalah admin maka system akan menampilkan menu admin
+      // Jika user bukan admin maka system akan menampilkan menu user
       if (userData.isUserAdmin()) isRunning = adminMenu(&choice, &index, &page, &pagination, result);
       else isRunning = userMenu(&choice, &index, &page, &pagination, result);
-    } while (isRunning);
+    } while (isRunning); // Melakukan perulangan selama isRunning bernilai true
   }
 };
 
--- controllers/user_controller.hpp
+++ controllers/user_controller.hpp
@@ -38,87 +38,208 @@ using std::string;
  */
 class UserController {
 private:
+  // Deklarasi variabel yang akan digunakan
+  // untuk menyimpan data username dan password
+  // dengan tipe data string
   string username, password;
+
+  // Deklarasi variabel yang akan digunakan
+  // untuk menyimpan data user yang sedang login
   UserStruct currentUser;
+
+  // Deklarasi variabel yang akan digunakan
+  // untuk menyimpan data user yang sudah terdaftar
   List<UserStruct> userList;
 
+  /**
+   * @brief Method untuk menampilkan form dasar
+   * 
+   * @return void
+   */
   void basic_form() {
+    // Cek apakah user sudah login
+    // jika sudah, maka tampilkan pesan anda sudah login
     if (isAlreadyLogin()) {
+      // Mencetak pesan ke layar terminal
       cout << "Anda sudah masuk sebagai " << currentUser.username << endl;
+
+      // Menghentikan eksekusi kode
       return;
     }
 
+    // Meminta inputan dari user berupa username
     cout << "Masukan nama pengguna : ";
     cin >> username;
 
+    // Meminta inputan dari user berupa password
     cout << "Masukan kata sandi : ";
     cin >> password;
   }
 
+  /**
+   * @brief Method untuk memproses autentikasi user
+   * 
+   * @return void
+   */
   void process_signin() {
+    // Deklarasi variabel result dan tempUser
+    // yang akan digunakan untuk menyimpan data user
+    // yang sudah terdaftar dan data user yang akan
+    // di autentikasi
     UserStruct result, tempUser = {username, password, Guest};
+
+    // Memanggil method findNodeData pada userList dengan
+    // parameter 2 dan tempUser, lalu menyimpan hasilnya
+    // ke dalam variabel result
     result = userList.findNodeData(2, tempUser);
 
+    // Cek apakah username dan password kosong
+    // jika kosong, maka tampilkan pesan akun tidak ditemukan
     if (result.username.empty() || result.password.empty()) {
+      // Mencetak pesan ke layar terminal
       cout << "Akun tidak ditemukan, silahkan registrasi" << endl;
+
+      // Menghentikan eksekusi kode
       return;
     }
 
+    // Membandingkan password yang diinputkan
+    // oleh user dengan password yang sudah tersimpan
+    // Jika password tidak sama, maka tampilkan pesan
+    // kata sandi salah, silahkan coba lagi
     if (!Hashing::verify(password, result.password)) {
+      // Mencetak pesan ke layar terminal
       cout << "Kata sandi salah, silahkan coba lagi" << endl;
+
+      // Menghentikan eksekusi kode
       return;
     }
 
+    // Menyimpan data user yang sedang login
     currentUser = result;
+
+    // Mencetak pesan ke layar terminal
     cout << "Proses autentikasi berhasil, selamat datang " << result.username << endl;
   }
 
+  /**
+   * @brief Method untuk memproses registrasi user
+   * 
+   * @return void
+   */
   void process_signup() {
+    // Deklarasi variabel result dan tempUser
+    // yang akan digunakan untuk menyimpan data user
+    // yang sudah terdaftar dan data user yang akan
+    // di autentikasi
     UserStruct result, tempUser = {username, password, Guest};
+
+    // Memanggil method findNodeData pada userList dengan
+    // parameter 2 dan tempUser, lalu menyimpan hasilnya
+    // ke dalam variabel result
     result = userList.findNodeData(2, tempUser);
 
+    // Cek apakah username dan password kosong
+    // jika tidak kosong, maka tampilkan pesan akun sudah ada
     if (!result.username.empty() || !result.password.empty()) {
+      // Mencetak pesan ke layar terminal
       cout << "Akun sudah ada, silahkan gunakan akun yang lain" << endl;
+
+      // Menghentikan eksekusi kode
       return;
     }
 
+    // Mengubah password yang diinputkan oleh user
+    // menjadi password yang sudah dienkripsi
     tempUser.password = Hashing::encrypt(password);
+
+    // Menambahkan data user yang baru ke dalam userList
     userList.push(tempUser);
+
+    // Menyimpan data user yang baru ke dalam file user_data.csv
     UserModel::insert(tempUser);
+
+    // Menyimpan data user yang sedang login
     currentUser = tempUser;
 
+    // Mencetak pesan ke layar terminal
     cout << "Akun berhasil di buat, silahkan autentikasi akun anda" << endl;
   }
 
+  /**
+   * @brief Method untuk menampilkan menu autentikasi
+   * 
+   * @return void
+   */
   void signin() {
+    // Memanggil method basic_form untuk menampilkan form dasar
     basic_form();
+
+    // Memanggil method process_signin untuk memproses autentikasi user
     process_signin();
+
+    // Menampilkan pesan ke layar terminal
     system("pause");
   }
 
+  /**
+   * @brief Method untuk menampilkan menu registrasi
+   * 
+   * @return void
+   */
   void signup() {
+    // Memanggil method basic_form untuk menampilkan form dasar
     basic_form();
 
+    // Meminta inputan dari user berupa konfirmasi password
     string password_confirmation;
 
+    // Meminta inputan dari user berupa konfirmasi password
     cout << "Masukan ulang kata sandi : ";
     cin >> password_confirmation;
 
+    // Membandingkan password yang diinputkan
+    // oleh user dengan konfirmasi password
+    // Jika password tidak sama, maka tampilkan pesan
+    // kata sandi tidak sama, silahkan coba lagi
     if (password != password_confirmation) {
+      // Mencetak pesan ke layar terminal
       cout << "Kata sandi tidak sama, silahkan coba lagi" << endl;
+
+      // Menghentikan eksekusi kode
       return;
     }
 
+    // Memanggil method process_signup untuk memproses registrasi user
     process_signup();
+
+    // Menampilkan pesan ke layar terminal
     system("pause");
   }
 
+  /**
+   * @brief Method untuk logout
+   * 
+   * @return void
+   */
   void logout() {
+    // Menghapus data user yang sedang login
     currentUser = UserStruct{};
+
+    // Menampilkan pesan ke layar terminal
     cout << "Anda berhasil keluar" << endl;
+
+    // Menampilkan pesan ke layar terminal
     system("pause");
   }
 
+  /**
+   * @brief Method untuk menampilkan menu user
+   * 
+   * @param choice
+   * 
+   * @return bool
+   */
   bool guestMenu(int *choice) {
     cout << "List of User Menu" << endl;
     cout << "===================" << endl;
@@ -129,28 +250,56 @@ private:
     cout << "Pilih menu : ";
     cin >> *choice;
 
+    // Cek apakah pilihan user tidak valid
+    // Jika pilhan user kurang dari 1 atau lebih dari 3
+    // maka system akan mencetak pesan pilihan tidak valid
     if (*choice < 1 || *choice > 3) {
+      // Mencetak pesan pilihan tidak valid
       cout << "Pilihan tidak valid" << endl;
+
+      // Menunggu user untuk menekan tombol apapun
       system("pause");
     }
 
+    // Jika user memilih menu 3
     if (*choice == 3) return false;
+
+    // Menghapus layar pada terminal
     system("cls");
 
+    // Melakukan pengecekan pilihan user
     switch (*choice) {
+    // Jika user memilih menu 1
     case 1:
+      // Memanggil method signin
       signin();
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih menu 2
     case 2:
+      // Memanggil method signup
       signup();
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih selain 1, 2
     default:
+      // Menghentikan pengecekan
       break;
     }
 
+    // Mengembalikan nilai true
     return true;
   }
 
+  /**
+   * @brief Method untuk menampilkan menu user
+   * 
+   * @param choice
+   * 
+   * @return bool
+   */
   bool userMenu(int *choice) {
     cout << "List of User Menu" << endl;
     cout << "===================" << endl;
@@ -160,68 +309,134 @@ private:
     cout << "Pilih menu : ";
     cin >> *choice;
 
+    // Cek apakah pilihan user tidak valid
+    // Jika pilhan user kurang dari 1 atau lebih dari 2
+    // maka system akan mencetak pesan pilihan tidak valid
     if (*choice < 1 || *choice > 2) {
+      // Mencetak pesan pilihan tidak valid
       cout << "Pilihan tidak valid" << endl;
+
+      // Menunggu user untuk menekan tombol apapun
       system("pause");
     }
 
+    // Jika user memilih menu 3
     if (*choice == 2) return false;
+
+    // Menghapus layar pada terminal
     system("cls");
 
+    // Melakukan pengecekan pilihan user
     switch (*choice) {
+    // Jika user memilih menu 1
     case 1:
-      currentUser = UserStruct{};
-      cout << "Anda berhasil keluar" << endl;
+      // Memanggil method logout
+      logout();
+
+      // Menunggu user untuk menekan tombol apapun
       system("pause");
+
+      // Menghentikan pengecekan
       break;
+    // Jika user memilih selain 1
     default:
+      // Menghentikan pengecekan
       break;
     }
 
+    // Mengembalikan nilai true
     return true;
   }
 
 public:
+  /**
+   * @brief Method untuk menginisialisasi data user
+   * 
+   * @return void
+   */
   void init() {
+    // Mengecek apakah file user_data.csv tidak ada
+    // Jika tidak ada maka system akan membuat file user_data.csv
+    // dan menambahkan data destinasi secara default
     if (!FileStorage::Exists(Path::getPath() + "/data/user_data.csv")) {
+      // Membuat file user_data.csv
+      FileStorage::Create(Path::getPath() + "/data/user_data");
+
+      // Menambahkan data user secara default
       UserModel::insert(UserStruct{"user", Hashing::encrypt("password"), Guest});
       UserModel::insert(UserStruct{"admin", Hashing::encrypt("password"), Admin});
     }
 
+    // Mengecek apakah data destinasi kosong pada linked list
+    // Jika kosong maka system akan membaca data dari file csv
     if (userList.isEmpty()) {
+      // Membaca data dari file csv
       std::vector<UserStruct> data = UserModel::read();
 
+      // Melakukan perulangan untuk menambahkan data ke dalam linked list
+      // berdasarkan data yang sudah di baca dari file csv
       for (int i = 0; i < data.size(); i++) {
+        // Menambahkan data ke dalam linked list
         userList.push(data[i]);
       }
     }
   }
 
+  /**
+   * @brief Method untuk mengecek apakah user sudah login
+   * 
+   * @return bool
+   */
   bool isAlreadyLogin() {
+    // Cek apakah username dan password kosong
+    // Jika kosong, maka system akan mengembalikan nilai false
     if (currentUser.username.empty() || currentUser.password.empty())
+      // Mengembalikan nilai false
       return false;
 
+    // Mengembalikan nilai true
     return true;
   }
 
+  /**
+   * @brief Method untuk mengecek apakah user adalah admin
+   * 
+   * @return bool
+   */
   bool isUserAdmin() {
+    // Memanggil method isAlreadyLogin untuk mengecek apakah user sudah login
     const bool isLogged = isAlreadyLogin();
 
+    // Cek apakah user sudah login
+    // Jika belum login, maka system akan mengembalikan nilai false
     if (!isLogged) return false;
+
+    // Cek apakah role user adalah admin
     if (currentUser.role == Admin) return true;
 
+    // Mengembalikan nilai false
     return false;
   }
 
+  /**
+   * @brief Method untuk menampilkan menu autentikasi
+   * 
+   * @return void
+   */
   void menu() {
+    // Deklarasi variabel untuk menyimpan pilihan user
     int choice, isRunning = 1;
 
     do {
+      // Menghapus layar pada terminal
       system("cls");
 
+      // Cek apakah user sudah login
+      // Jika sudah, maka tampilkan pesan anda sudah login
+      // Jika belum, maka tampilkan menu user
       if (isAlreadyLogin()) isRunning = userMenu(&choice);
       else isRunning = guestMenu(&choice);
-    } while (isRunning);
+    } while (isRunning); // Melakukan perulangan selama isRunning bernilai true
   }
 };
 
