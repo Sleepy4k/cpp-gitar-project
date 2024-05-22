51b2c588277fb061e7c32c0b985537beb9033702
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-05-22 08:06:44 +0700

    chore(model): change read method for package

--- models/package_model.hpp
+++ models/package_model.hpp
@@ -10,6 +10,7 @@
 #include "../helpers/path.hpp"
 #include "../handlers/data_handler.hpp"
 #include "../structs/package_struct.hpp"
+#include "../enums/destination_type_enum.hpp"
 #include "../enums/package_transport_enum.hpp"
 
 // Path file data destinasi wisata
@@ -32,7 +33,7 @@ namespace PackageModel {
    * 
    * @return std::vector<PackageStruct>   
    */
-  std::vector<PackageStruct> read(int category = 1) {
+  std::vector<PackageStruct> read() {
     // Menggunakan std::ifstream untuk membaca data dari file
     using std::ifstream; // Import library ifstream
     // Menggunakan std::stringstream untuk mengkonversi data ke dalam string
@@ -41,12 +42,17 @@ namespace PackageModel {
     // Membaca data dari file dengan menggunakan FileStorage::Read
     std::string fileData = FileStorage::Read(Path::getPath() + PACKAGE_DATA_PATH);
 
+    // Inisialisasi random seed
+    // untuk menghasilkan angka random yang berbeda
+    // setiap kali program dijalankan
+    srand(time(NULL));
+
     // Inisialisasi variabel line untuk menyimpan data yang di baca
     std::string line;
 
     // Inisialisasi variable length untuk menyimpan jumlah data
     // dalam satu paket destinasi wisata
-    const int length = rand() % 3 + 1;
+    const int length = (rand() % 4) + 2;
 
     // Inisialisasi variabel index untuk menyimpan index data
     // Jika index lebih dari length maka index akan di reset
@@ -61,14 +67,22 @@ namespace PackageModel {
     // Menggunakan std::stringstream untuk mengkonversi data ke dalam string
     stringstream ss(fileData);
 
-    // Get total data
+    // Menghitung total data yang ada pada file
     while (std::getline(ss, line, '\n')) {
+      // Jika pada baris tersebut tidak terdapat data "\n" maka akan di skip
+      // Jika ada maka akan di tambahkan ke dalam total
       total++;
     }
 
+    // Mengubah nilai gap menjadi total
     gap = total;
+
+    // Mengubah nilai gap menjadi gap dikurangi total modulo length
+    // Menghindari data yang tidak lengkap pada paket destinasi wisata
     gap -= total % length;
 
+    // Menggunakan std::stringstream untuk mengkonversi data ke dalam string
+    // karena ss sudah di gunakan sebelumnya maka kita harus membuat ulang
     ss = stringstream(fileData);
 
     // Melakukan perulangan untuk membaca data per baris
@@ -88,24 +102,21 @@ namespace PackageModel {
         row.push_back(word);
       }
 
-      // Mengubah data dari string ke dalam tipe data yang sesuai
-      // Jika data row sama dengan category yang diminta
-      if (row[7] == std::to_string(category)) {
-        // Mengisi data struct PackageStruct dengan data dari file
-        package.name = row[0];
-        package.description = row[0];
-        package.duration = 0;
-        package.price = std::stoi(row[4]);
-        package.people = std::stoi(row[5]);
-        package.transport = static_cast<PackageTransport>(rand() % 3 + 1);
-        package.facility = getListOfFacility(package.transport);
+      // Mengisi data struct PackageStruct dengan data dari file
+      package.name = row[0];
+      package.description = row[0];
+      package.duration = 0;
+      package.price = std::stoi(row[4]);
+      package.people = std::stoi(row[5]);
+      package.type = static_cast<DestinationType>(std::stoi(row[7]));
+      package.transport = static_cast<PackageTransport>((rand() % 3) + 1);
+      package.facility = getListOfFacility(package.transport);
 
-        // Menambahkan data ke dalam variabel data
-        temp.push_back(package);
+      // Menambahkan data ke dalam variabel data
+      temp.push_back(package);
 
-        // Menambah nilai index
-        index++;
-      }
+      // Menambah nilai index
+      index++;
 
       // Membersihkan data pada row
       row.clear();
@@ -118,7 +129,7 @@ namespace PackageModel {
     total = index;
 
     // Inisialisasi variabel highest untuk menyimpan nilai tertinggi
-    int highest = 0, inc = 0, each = total / length;
+    int highest = 0, inc = 0, each = length;
 
     // Melakukan manipulasi data pada temp paket destinasi wisata
     // dengan logika yang sudah di tentukan dibawah
@@ -135,28 +146,36 @@ namespace PackageModel {
       package.people = 0;
       package.name = "Paket ";
       package.description = "Lokasi wisata yang dikunjungi : ";
-      package.duration = rand() % 3 + 1;
+      package.duration = (rand() % 3) + 1;
 
       // Mengganti nilai index menjadi 0
-      index = 0 + inc;
+      index = inc;
 
       // Mengubah data dari string ke dalam tipe data yang sesuai
-      for (int i = 0; i <= each; i++) {
+      for (int i = 0; i < each; i++) {
+        // Menambahkan data ke dalam variabel package
         package.name += toupper(temp[i + index].name[0]);
         package.description += temp[i + index].description;
         package.people += temp[i + index].people;
         package.price += temp[i + index].price;
-        
+
+        // Jika paket transportasi tertinggi dari sebelumnya
+        // maka variable highest akan di ganti dengan nilai tersebut
         if (temp[i + index].transport > highest) {
           highest = temp[i + index].transport;
         }
 
-        if (i < each) package.description += ", ";
-        if (index < each) index += each;
+        // Jika i kurang dari each - 1 maka akan menambahkan koma
+        if (i < each - 1) package.description += ", ";
       }
 
+      // Mengubah harga paket dengan mengalikan harga dengan durasi
       package.price = package.price * package.duration;
+
+      // Mengubah data dari integer ke dalam tipe data yang sesuai
       package.transport = static_cast<PackageTransport>(highest);
+
+      // Menambahkan fasilitas berdasarkan transportasi tertinggi
       package.facility = getListOfFacility(highest);
 
       // Menambahkan data ke dalam variabel data
@@ -164,8 +183,9 @@ namespace PackageModel {
 
       // Mengurangi nilai dari variable total
       total -= length;
-      
-      inc++;
+
+      // Menambah nilai inc dengan length - 1
+      inc += length - 1;
     }
 
     // Mengembalikan data
