02da94575fefb40133ea8f69b77f7430c2deab3a
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-05-25 01:50:38 +0700

    feat(struct): add hash table and reciept struct

---
+++ structs/hash_table_struct.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifndef HASH_TABLE_STRUCT_HPP
+#define HASH_TABLE_STRUCT_HPP
+
+#include <string>
+
+/**
+ * Deklarasi struct HashTable
+ *
+ * Struct hash table ini akan kita gunakan
+ * untuk menyimpan data pada hash table, pada
+ * struct ini terdapat dua field yaitu :
+ * - key = untuk menyimpan key dari data yang
+ *         akan kita masukkan ke dalam hash table
+ * - value = untuk menyimpan value dari data
+ *           yang akan kita masukkan ke dalam
+ *           hash table
+ *
+ * Note : Struct adalah tipe data yang dapat
+ *        menyimpan beberapa tipe data dan
+ *        dapat digunakan untuk membuat object
+ */
+template <typename T>
+struct HashTableStruct {
+  std::string key;
+  T value;
+};
+
+#endif // HASH_TABLE_STRUCT_HPP
---
+++ structs/reciept_struct.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifndef RECIEPT_STRUCT_HPP
+#define RECIEPT_STRUCT_HPP
+
+#include <string>
+
+/**
+ * Deklarasi struct Reciept
+ *
+ * Struct reciept ini akan kita gunakan
+ * untuk merapihkan data reciept, untuk
+ * field pada struct yaitu :
+ * - name     = untuk menyimpan nama
+ *              dari customer
+ * - phone    = untuk menyimpan nomor
+ *              telepon dari customer
+ * - package  = untuk menyimpan nama
+ *              paket yang dipilih
+ * - date     = untuk menyimpan tanggal
+ *              pemesanan
+ * - duration = untuk menyimpan durasi
+ *              dari paket
+ * - price    = untuk menyimpan harga
+ *              dari paket
+ * - people   = untuk menyimpan jumlah
+ *              orang dalam satu paket
+ *
+ * Note : Struct adalah tipe data yang dapat
+ *        menyimpan beberapa tipe data dan
+ *        dapat digunakan untuk membuat object
+ */
+struct RecieptStruct {
+  std::string name, phone, package, date;
+  int duration, price, people;
+};
+
+#endif // RECIEPT_STRUCT_HPP
