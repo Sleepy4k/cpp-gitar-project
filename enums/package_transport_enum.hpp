e54b7356c8cc842bd04e60ca28811efac637cc7b
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-05-29 14:47:18 +0700

    chore(enum): add code documentation

--- enums/package_transport_enum.hpp
+++ enums/package_transport_enum.hpp
@@ -30,15 +30,25 @@ enum PackageTransport {
  * @return string
  */
 std::string getFacilityName(int transport) {
+  // Menggunakan switch case untuk mengecek
+  // nilai dari transport dan mengembalikan string
   switch (transport) {
-    case 1:
-      return "Bus";
-    case 2:
-      return "Kereta Api";
-    case 3:
-      return "Pesawat Terbang";
-    default:
-      return "Tidak Diketahui";
+  // Jika transport sama dengan 1
+  case 1:
+    // Mengembalikan string "Bus"
+    return "Bus";
+  // Jika transport sama dengan 2
+  case 2:
+    // Mengembalikan string "Kereta Api"
+    return "Kereta Api";
+  // Jika transport sama dengan 3
+  case 3:
+    // Mengembalikan string "Pesawat Terbang"
+    return "Pesawat Terbang";
+  // Jika tidak ada case yang sesuai
+  default:
+    // Mengembalikan string "Tidak Diketahui"
+    return "Tidak Diketahui";
   }
 }
 
@@ -50,15 +60,25 @@ std::string getFacilityName(int transport) {
  * @return string
  */
 std::string getListOfFacility(int transport) {
+  // Menggunakan switch case untuk mengecek
+  // nilai dari transport dan mengembalikan string
   switch (transport) {
-    case 1:
-      return "Bus AC, TV, Pemamdu Wisata, Makanan dan Minuman, Toilet, dan Bagasi";
-    case 2:
-      return "Kereta Api Ekonomi, AC, TV, Makanan dan Minuman, Toilet, dan Bagasi";
-    case 3:
-      return "Pesawat Terbang Ekonomi, AC, TV, Makanan dan Minuman, Toilet, dan Bagasi";
-    default:
-      return "Tidak Diketahui";
+  // Jika transport sama dengan 1
+  case 1:
+    // Mengembalikan string "Bus AC, TV, Pemamdu Wisata, Makanan dan Minuman, Toilet, dan Bagasi"
+    return "Bus AC, TV, Pemamdu Wisata, Makanan dan Minuman, Toilet, dan Bagasi";
+  // Jika transport sama dengan 2
+  case 2:
+    // Mengembalikan string "Kereta Api Ekonomi, AC, TV, Makanan dan Minuman, Toilet, dan Bagasi"
+    return "Kereta Api Ekonomi, AC, TV, Makanan dan Minuman, Toilet, dan Bagasi";
+  // Jika transport sama dengan 3
+  case 3:
+    // Mengembalikan string "Pesawat Terbang Ekonomi, AC, TV, Makanan dan Minuman, Toilet, dan Bagasi"
+    return "Pesawat Terbang Ekonomi, AC, TV, Makanan dan Minuman, Toilet, dan Bagasi";
+  // Jika tidak ada case yang sesuai
+  default:
+    // Mengembalikan string "Tidak Diketahui"
+    return "Tidak Diketahui";
   }
 }
 
