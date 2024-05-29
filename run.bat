179b3ff07048bc8c34c1121f0a31da071ca32c63
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-05-29 14:04:41 +0700

    chore(run.bat): change command and script

--- run.bat
+++ run.bat
@@ -1,15 +1,15 @@
 echo off
 
 REM Call g++ to compile file source name
-g++ -std=c++11 "main.cpp" -o "main.exe"
+g++ -std=c++11 "main.cpp" -o "main"
 
 REM Check if compiling status is success
 if %errorlevel% equ 0 (
   REM If success then we print compiling is success and run it
   echo Compiling success, Trying to run main.exe
 
-  REM Run main.exe
-  main.exe
+  REM Run main
+  "./main"
 ) else (
   echo Compiling error, something went wrong
 )
