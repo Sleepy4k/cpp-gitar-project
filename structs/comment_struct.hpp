19eceadb76230098510516fe85c1cc284ac7eed6
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-05-29 22:54:41 +0700

    feat(struct): add comment struct

---
+++ structs/comment_struct.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef COMMENT_STRUCT_HPP
+#define COMMENT_STRUCT_HPP
+
+#include <string>
+
+/**
+ * Deklarasi struct Comment
+ *
+ * Struct comment ini akan kita gunakan
+ * untuk merapihkan data komentar,
+ * untuk field pada struct yaitu :
+ * - destination_name = untuk menyimpan nama destinasi
+ * - username         = untuk menyimpan username
+ * - comment          = untuk menyimpan komentar
+ * - rating           = untuk menyimpan rating
+ *
+ * Note : Struct adalah tipe data yang dapat
+ *        menyimpan beberapa tipe data dan
+ *        dapat digunakan untuk membuat object
+ */
+struct CommentStruct {
+  std::string destination_name, username, comment;
+  int rating;
+};
+
+#endif // COMMENT_STRUCT_HPP
